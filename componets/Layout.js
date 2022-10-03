4ede54455fae1631fa5919f296994e6e1b0db73c
Daniel Sperling <daniel_sperling@yahoo.com>
2022-10-03 14:14:44 +0300

    adding navbar

---
+++ componets/Layout.js
@@ -0,0 +1,11 @@
+import styles from '../styles/Layout.module.css'
+
+const name = (props) => {
+    return (
+        <div className={styles.container}>
+
+        </div>
+    )
+};
+
+export default name;
--- pages/index.js
+++ pages/index.js
@@ -1,45 +1,81 @@
 import Head from 'next/head'
 import Image from 'next/image'
-import styles from '../styles/Home.module.css'
+import styles from '../styles/Layout.module.css'
 import first from '../public/first.jpg'
 
-const MyImage = (props) => {
+
+
+export default function Home() {
   return (
-    <Image
-      src={first}
-      alt="Picture of the author"
-      width="350px"
-      height="300px"
-    />
+
+    <div>
+      <Head>
+        <title> androbik Photography </title>
+
+      </Head>
+
+      <h1>Hello World</h1>
+
+    </div>
+
+
   )
 
-  export default function Home() {
-    return (
-      <div className='main'>
-
-        <div className='title'><h1>Nikolas - Petros Androvik</h1> </div>
-
-        <div className='main_menu'>
-          <h2>Home</h2>
-          <br />
-          <h2>German Bodybuilding - Vice</h2>
-          <h2>Medusa - HUnger Magazine</h2>
-          <h2>Dress to Kill - OffTheRails Magazine</h2>
-          <h2>Eagle Noir - Numero Berlin</h2>
-          <h2>The Edge - Contributor Magazine</h2>
-          <h2>I Dance Alone - Dscene Magazine</h2>
-          <h2>YNRY - Lookboock </h2>
-        </div>
-
-
-
-      </div>
+}
 
 
 
 
-    )
-
-  }
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+{/* <div className='main'>
+
+  <div className='title'><h1>Nikolas - Petros Androvik</h1>
+  </div>
+
+  <div className='main_menu'>
+    <h2>Home</h2>
+    <br />
+    <h2>German Bodybuilding - Vice</h2>
+    <h2>Medusa - HUnger Magazine</h2>
+    <h2>Dress to Kill - OffTheRails Magazine</h2>
+    <h2>Eagle Noir - Numero Berlin</h2>
+    <h2>The Edge - Contributor Magazine</h2>
+    <h2>I Dance Alone - Dscene Magazine</h2>
+    <h2>YNRY - Lookboock </h2>
+  </div>
+
+  <br />
+
+  <div className='main_menu_contact'>
+    <h2>contact/order</h2>
+    <h2> IG @adrobik</h2>
+  </div>
+
+</div> */}
