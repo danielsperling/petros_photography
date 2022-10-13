40f50ec739b1f3316fd08205963232234f988a4e
Daniel Sperling <daniel_sperling@yahoo.com>
2022-10-13 20:46:42 +0300

    vice images 2

--- pages/vice.js
+++ pages/vice.js
@@ -13,43 +13,48 @@ function UncontrolledExample() {
 			<Carousel.Item>
 				<img
 					className={carousel.image}
-					src="../vice/20-min.jpg"
+					src="../vice/0.jpg"
 					alt="First slide"
 				/>
-				<Carousel.Caption>
-					<h3>First slide label</h3>
-					<p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
-				</Carousel.Caption>
 			</Carousel.Item>
 			<Carousel.Item
 			>
 				<img
 					className={carousel.image}
-					src="https://images.unsplash.com/photo-1665406857456-0275f9517752?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=774&q=80"
-					alt="Second slide"
+					src="../vice/1.jpg"
+					alt="First slide"
 				/>
-
-				<Carousel.Caption>
-					<h3>Second slide label</h3>
-					<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-				</Carousel.Caption>
 			</Carousel.Item>
 			<Carousel.Item
-			// className={carousel.image}
-			>
+>
 				<img
 					className={carousel.image}
-					src="https://images.unsplash.com/photo-1664640525110-69d0ab715725?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2787&q=80"
-					alt="Third slide"
+					src="../vice/2.jpg"
+					alt="First slide"
 				/>
-
-				<Carousel.Caption>
-					<h3>Third slide label</h3>
-					<p>
-						Praesent commodo cursus magna, vel scelerisque nisl consectetur.
-					</p>
-				</Carousel.Caption>
 			</Carousel.Item>
+
+			<Carousel.Item>
+				<img
+					className={carousel.image}
+					src="../vice/3.jpg"
+					alt="First slide"
+				/>
+			</Carousel.Item>	
+			<Carousel.Item>
+				<img
+					className={carousel.image}
+					src="../vice/4.jpg"
+					alt="First slide"
+				/>
+			</Carousel.Item>
+			<Carousel.Item>
+				<img
+					className={carousel.image}
+					src="../vice/5.jpg"
+					alt="First slide"
+				/>
+			</Carousel.Item>		
 		</Carousel>
 	);
 }
